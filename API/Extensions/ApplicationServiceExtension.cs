9e7271f5bf4295712801b570fbdd49283fceae6f
TFernezir <tfernezir@conet.hr>
2024-11-14 11:54:10 +0100

    End of section 4

---
+++ API/Extensions/ApplicationServiceExtension.cs
@@ -0,0 +1,32 @@
+﻿using Application.Activities;
+using Application.Core;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Extensions;
+
+public static class ApplicationServiceExtension
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+    {
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddSwaggerGen();
+        services.AddDbContext<DataContext>(opt =>
+        {
+            opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+        });
+
+        services.AddCors(opt =>
+        {
+            opt.AddPolicy("CorsPolicy", policy =>
+            {
+                policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+            });
+        });
+
+        services.AddMediatR(config => config.RegisterServicesFromAssemblies(typeof(List.Handler).Assembly));
+        services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+        return services;
+    }
+}
