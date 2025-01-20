ad8fac70c0477fc06ab28f168868503acff2b790
TFernezir <tfernezir@conet.hr>
2025-01-20 14:46:02 +0100

    fallback controller (standard stuff) and wwwroot (created by npm run buld)

--- API/Program.cs
+++ API/Program.cs
@@ -37,8 +37,12 @@ app.UseCors("CorsPolicy");
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 app.MapControllers();
 app.MapHub<ChatHub>("/chat");
+app.MapFallbackToController("Index", "FallBack");
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
